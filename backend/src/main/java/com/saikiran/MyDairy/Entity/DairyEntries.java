5cea5e48697d07d0fd5e410fedc4944566b0f7d6
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-07-22 12:15:49 +0530

    update configuration

--- backend/src/main/java/com/saikiran/MyDairy/Entity/DairyEntries.java
+++ backend/src/main/java/com/saikiran/MyDairy/Entity/DairyEntries.java
@@ -1,6 +1,11 @@
 package com.saikiran.MyDairy.Entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
